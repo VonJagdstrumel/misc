78e86cc150e8859a6246dcf85c05ec3797db9d8b
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-04-06 12:46:45 +0200

    Added cygwin_setup.cmd

---
+++ code/batch/cygwin_setup.cmd
@@ -0,0 +1,5 @@
+setup-x86_64.exe ^
+-K http://cygwinports.org/ports.gpg ^
+-P attr,bash-completion,curl,cvs,cygwin-devel,dos2unix,file-devel,gdb,gettext,info,libasn1_8,libcloog0,libgssapi3,libguile17,libheimbase1,libheimntlm0,libhx509_5,libiconv,libkafs0,libkrb5_26,libltdl7,libmetalink3,libpng-doc,libpng16,libppl9,libppl_c4,libpwl5,libreadline-devel,libroken18,libsmartcols1,libuuid-devel,libwind0,lua,lua-bit,lua-crypto,lua-json,lua-lfs,lua-logging,lua-lpeg,lua-socket,man,mingw-gcc-core,mingw-gcc-g++,mingw-pthreads,mingw-runtime,mingw-w32api,openssl-devel,patch,perl_autorebase,perl_base,texinfo,time,upx,zlib-devel,zsh,ruby ^
+-x libXss1,perl-Term-ReadKey,python-tkinter,tcl-tix,tcl-tk ^
+-g -o
