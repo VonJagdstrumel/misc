3d3181f33efa9f12ee6b664e122522f086d35bd2
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-05-09 03:02:07 +0200

    Updated vagrant_debian.sh to Jessie + some improvements

--- code/bash/vagrant_debian.sh
+++ code/bash/vagrant_debian.sh
@@ -5,19 +5,21 @@ echo 'UseDNS no' >> /etc/ssh/sshd_config
 echo '' > /etc/motd
 service ssh restart
 
+# Disable GRUB boot menu
+sed -i -r 's/^GRUB_TIMEOUT=[0-9]+$/GRUB_TIMEOUT=0/' /etc/default/grub
+update-grub
+
 # Configure aptitude to use dotdeb and backports
 wget http://www.dotdeb.org/dotdeb.gpg
 apt-key add dotdeb.gpg
 rm dotdeb.gpg
 cat <<EOF >> /etc/apt/sources.list
 
-deb http://ftp.debian.org/debian/ wheezy-backports main
-deb-src http://ftp.debian.org/debian/ wheezy-backports main
+deb http://ftp.debian.org/debian/ jessie-backports main
+deb-src http://ftp.debian.org/debian/ jessie-backports main
 
-deb http://packages.dotdeb.org/ wheezy all
-deb-src http://packages.dotdeb.org/ wheezy all
-deb http://packages.dotdeb.org/ wheezy-php56 all
-deb-src http://packages.dotdeb.org/ wheezy-php56 all
+deb http://packages.dotdeb.org/ jessie all
+deb-src http://packages.dotdeb.org/ jessie all
 EOF
 
 # Lower timeout for aptitude
@@ -32,12 +34,16 @@ apt-get -y autoclean
 apt-get -y autoremove
 
 # Install required software
-apt-get -y install binutils build-essential chkconfig curl dfc dkms gawk git htop iftop iptraf linux-headers-amd64 nethogs rcconf screen subversion sudo sysstat unzip
-apt-get -y install lua luarocks
-apt-get -y install php-pear php5-cli php5-common php5-curl php5-fpm php5-gd php5-gmp php5-mcrypt php5-mysqlnd php5-pgsql php5-readline php5-sqlite php5-xsl
+apt-get -y install binutils build-essential chkconfig curl dkms gawk git htop iftop iptraf linux-headers-amd64 nethogs rcconf screen subversion sudo sysstat unzip
+apt-get -y install lua5.2 luarocks
+apt-get -y install php-pear php5-cli php5-common php5-curl php5-dev php5-fpm php5-gd php5-gmp php5-mcrypt php5-mysqlnd php5-pgsql php5-readline php5-sqlite php5-xdebug php5-xsl
 apt-get -y install postfix postgresql
 apt-get -y install python python-pip
-apt-get -y install ruby
+apt-get -y install ruby golang nodejs
+
+# Install Oracle VirtualBox Guest Additions
+mount /dev/cdrom /media/cdrom
+sh /media/cdrom/VBoxLinuxAdditions.run
 
 # Grant sudo to vagrant user
 echo '' >> /etc/sudoers
@@ -66,3 +72,5 @@ LP_ENABLE_BATT=0
 # Only load Liquid Prompt in interactive shells, not from a script or from scp
 [[ $- = *i* ]] && source ~/liquidprompt/liquidprompt
 EOF
+
+exit
