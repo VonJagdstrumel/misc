899cea9a830816f0a9add11fa2f03bb9d9715f1b
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-05-11 18:05:58 +0200

    Removed example and simplified implementation (similar to SplEnum's one)

--- code/php/enum_emulation.php
+++ code/php/enum_emulation.php
@@ -7,54 +7,31 @@
 
 abstract class Enum
 {
-    protected $value = null;
+    protected $value;
 
-    public function getValue()
+    /**
+     *
+     * @return mixed
+     */
+    function __toString()
     {
-        if (is_null($this->value)) {
-            throw new Exception('Enum value not set');
-        }
-
         return $this->value;
     }
 
-    public function setValue($value)
+    /**
+     *
+     * @param mixed $value
+     * @throws \UnexpectedValueException
+     */
+    function __construct($value)
     {
-        $reflection = new ReflectionClass(get_called_class());
+        $reflection = new \ReflectionClass(get_called_class());
         $constList = array_values($reflection->getConstants());
 
         if (!in_array($value, $constList)) {
-            throw new Exception('Trying to set an invalid enum value');
+            throw new \UnexpectedValueException('Trying to set an invalid enum value');
         }
 
         $this->value = $value;
     }
 }
-
-class EnumTest extends Enum
-{
-    const ZERO = 0;
-    const ONE = 1;
-    const TWO = 2;
-    const THREE = 3;
-
-}
-
-$enum = new EnumTest();
-
-try {
-    var_dump($enum->getValue()); // Will throw an exception
-} catch (Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
-}
-
-$enum->setValue(EnumTest::TWO);
-var_dump($enum->getValue());
-
-try {
-    $enum->setValue(42); // Will throw an exception
-} catch (Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
-}
-
-var_dump($enum->getValue());
