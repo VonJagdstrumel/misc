ca6a629edf6a5dd655c555f77ba7621088c8822f
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-05-11 18:05:08 +0200

    Added some locks on file streams

--- code/php/download_throttle.php
+++ code/php/download_throttle.php
@@ -4,21 +4,25 @@
  * Apply throttle on the download of a file
  */
 
-// YOU CAN TOUCH THIS SHIT
 $remote = @$argv[1] ? : exit;
 $local = @$argv[2] ? : exit;
 $bandwidth = @$argv[3] ? : exit;
 
-// DON'T TOUCH THIS SHIT NOW
 $bufferSize = 8192;
 $callsPerSecond = $bandwidth / $bufferSize;
 $microSleepTime = 1e6 / $callsPerSecond;
 
-$remoteHandle = fopen($remote, 'r');
-$localHandle = fopen($local, 'w+');
+$remoteHandle = fopen($remote, 'rb');
+$localHandle = fopen($local, 'wb');
+
+flock($remoteHandle, LOCK_SH);
+flock($localHandle, LOCK_EX);
 
 while (!feof($remoteHandle)) {
     $buffer = fread($remoteHandle, $bufferSize);
     fwrite($localHandle, $buffer);
     usleep($microSleepTime);
 }
+
+fclose($remoteHandle);
+fclose($localHandle);
