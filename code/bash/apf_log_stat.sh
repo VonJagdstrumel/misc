3d0d3707be04b53ca89b6838e1b5c4592f060426
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-08-04 19:28:40 +0200

    Added apf_log_stat.sh

---
+++ code/bash/apf_log_stat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Aggregate port access stats from APF log files
+
+cat apf_drops* | \
+egrep 'PROTO=(UDP|TCP|2)' | \
+perl -pe 's/^.*PROTO=(UDP|TCP|2)(?: SPT=(?:[0-9]+) DPT=([0-9]+))?.*$/($1==2?"ICMP":$1).($2?",".$2:"")/eg' | \
+/bin/sort | \
+uniq -c | \
+sed -r 's/^ +([0-9]+) /\1,/' | \
+/bin/sort -r -n
