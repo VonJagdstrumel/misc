9ad86d5982f42ed1ba4750730779d50f03ccdb8a
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-08-09 23:25:47 +0200

    Removed absolute path for find

--- code/bash/apf_log_stat.sh
+++ code/bash/apf_log_stat.sh
@@ -4,7 +4,7 @@
 cat apf_drops* | \
 egrep 'PROTO=(UDP|TCP|2)' | \
 perl -pe 's/^.*PROTO=(UDP|TCP|2)(?: SPT=(?:[0-9]+) DPT=([0-9]+))?.*$/($1==2?"ICMP":$1).($2?",".$2:"")/eg' | \
-/bin/sort | \
+sort | \
 uniq -c | \
 sed -r 's/^ +([0-9]+) /\1,/' | \
-/bin/sort -r -n
+sort -r -n
