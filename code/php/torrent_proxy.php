12ea2c40046d1c0b488912a26328efaaa83ad73d
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-12-29 00:10:10 +0100

    Updated comment from torrent_proxy.php

--- code/php/torrent_proxy.php
+++ code/php/torrent_proxy.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * Launch using 'php -S 127.0.0.1:8080 torrentproxy.php'
+ * Launch using 'php -S 127.0.0.1:8090 torrent_proxy.php'
  * Take the real tracker URL without the trailing 'announce' (e.g. http://example-tracker.net:8000/some-user-key/announce.php becomes http://example-tracker.net:8000/some-user-key/)
  * Encode in Base64 (e.g. aHR0cDovL2V4YW1wbGUtdHJhY2tlci5uZXQ6ODAwMC9zb21lLXVzZXIta2V5Lw)
- * Insert it in 'http://127.0.0.1:8080/<base64_encoded_tracker_url>/announce' (e.g. http://127.0.0.1:8080/aHR0cDovL2V4YW1wbGUtdHJhY2tlci5uZXQ6ODAwMC9zb21lLXVzZXIta2V5Lw/announce)
+ * Insert it in 'http://127.0.0.1:8090/<base64_encoded_tracker_url>/announce' (e.g. http://127.0.0.1:8090/aHR0cDovL2V4YW1wbGUtdHJhY2tlci5uZXQ6ODAwMC9zb21lLXVzZXIta2V5Lw/announce)
  * Use that URL in your torrents and remove the real one
  */
 
