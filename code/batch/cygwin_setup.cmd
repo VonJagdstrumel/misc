bca40a25a0f2ebe232d449fe02593303371a5ab4
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-12-29 00:09:49 +0100

    Removed redundants packages and added mingw64 and others from cygwin_setup.cmd

--- code/batch/cygwin_setup.cmd
+++ code/batch/cygwin_setup.cmd
@@ -1,8 +1,8 @@
 @echo off
 powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('http://cygwin.com/setup-x86_64.exe','setup-x86_64.exe')
 setup-x86_64.exe ^
--K http://cygwinports.org/ports.gpg ^
--P attr,bash-completion,bc,cmake,curl,cvs,cygwin-devel,dos2unix,file-devel,gdb,gettext,info,libasn1_8,libcloog0,libgssapi3,libguile17,libheimbase1,libheimntlm0,libhx509_5,libiconv,libkafs0,libkrb5_26,libltdl7,libmetalink3,libpng-doc,libpng16,libppl9,libppl_c4,libpwl5,libreadline-devel,libroken18,libsmartcols1,libuuid-devel,libwind0,lua,lua-bit,lua-crypto,lua-json,lua-lfs,lua-logging,lua-lpeg,lua-socket,man,mingw-gcc-core,mingw-gcc-g++,mingw-pthreads,mingw-runtime,mingw-w32api,openssl-devel,patch,perl_autorebase,perl_base,texinfo,time,upx,zlib-devel,zsh,ruby ^
--x libXss1,perl-Term-ReadKey,python-tkinter,tcl-tix,tcl-tk ^
--g -o
+-s ftp://ftp-stud.hs-esslingen.de/pub/Mirrors/sources.redhat.com/cygwin/ ^
+-P attr,bash-completion,bc,bind-utils,cmake,curl,cvs,diffutils,dos2unix,file-devel,gcc-g++,gdb,gettext,libboost-devel,lua-bit,lua-crypto,lua-json,lua-lfs,lua-logging,lua-lpeg,lua-socket,make,man-pages-posix,mingw-gcc-g++,mingw64-i686-gcc-g++,mingw64-x86_64-gcc-g++,nc,openssl-devel,patch,perl,ruby,time,upx,unzip,wget,zsh ^
+-l C:\Windows\Temp ^
+-D -L -q -n -g -o
 del setup-x86_64.exe
