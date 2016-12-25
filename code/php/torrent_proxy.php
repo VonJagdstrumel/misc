d1649ea9580a178ae9b105e2ce63ae1567498ad1
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2016-12-25 23:47:15 +0100

    Using Guzzle, headers proxy and simple error detection

--- code/php/torrent_proxy.php
+++ code/php/torrent_proxy.php
@@ -1,32 +1,64 @@
 <?php
 
 /*
- * Launch using 'php -S 127.0.0.1:8090 torrent_proxy.php'
- * Take the real tracker URL without the trailing 'announce' (e.g. http://example-tracker.net:8000/some-user-key/announce.php becomes http://example-tracker.net:8000/some-user-key/)
- * Encode in Base64 (e.g. aHR0cDovL2V4YW1wbGUtdHJhY2tlci5uZXQ6ODAwMC9zb21lLXVzZXIta2V5Lw)
- * Insert it in 'http://127.0.0.1:8090/<base64_encoded_tracker_url>/announce' (e.g. http://127.0.0.1:8090/aHR0cDovL2V4YW1wbGUtdHJhY2tlci5uZXQ6ODAwMC9zb21lLXVzZXIta2V5Lw/announce)
- * Use that URL in your torrents and remove the real one
+ * Take the real tracker URL (e.g. http://example-tracker.net:8000/some-user-key/announce)
+ * Remove the trailing '/announce' (e.g. http://example-tracker.net:8000/some-user-key)
+ * Encode in Base64 (e.g. aHR0cDovL2V4YW1wbGUtdHJhY2tlci5uZXQ6ODAwMC9zb21lLXVzZXIta2V5)
+ * Insert it in 'http://<your_server>/<base64>/announce' (e.g. http://127.0.0.1:8090/aHR0cDovL2V4YW1wbGUtdHJhY2tlci5uZXQ6ODAwMC9zb21lLXVzZXIta2V5/announce)
+ * Update your torrents
  */
 
-$requestUrlParts = explode('/', $_SERVER['PHP_SELF']);
-$trackerUrl = base64_decode($requestUrlParts[1]) . $requestUrlParts[2] . '?';
-$opts = array(
-    'http' => array(
-        'header' => "User-Agent: uTorrent/2210(25110)\r\nAccept-Encoding: gzip\r\nConnection: Close\r\n"
-    )
-);
+require 'vendor/autoload.php';
 
-foreach ($_GET as $getKey => $getValue) {
-    switch ($getKey) {
-        case 'downloaded':
-            $getValue = '0';
-            break;
-        case 'peer_id':
-            $getValue = preg_replace('/^-[^-]+/', '-UT2110', $getValue);
-            break;
-    }
+use \GuzzleHttp\Client;
+use \GuzzleHttp\Exception\RequestException;
 
-    $trackerUrlParts[] = rawurlencode($getKey) . '=' . rawurlencode($getValue);
+$paramList = $_GET;
+foreach($paramList as $key => &$value) {
+	switch($key) {
+		case 'downloaded':
+			$value = '0';
+			break;
+		case 'peer_id':
+			$value = preg_replace('/[^-]+/', 'UT2110', $value, 1);
+			break;
+	}
 }
 
-print file_get_contents($trackerUrl . implode('&', $trackerUrlParts), false, stream_context_create($opts));
+$requestUrlParts = explode('/', $_SERVER['PHP_SELF']);
+$action = array_pop($requestUrlParts);
+$baseUrl = base64_decode(array_pop($requestUrlParts));
+$queryString = http_build_query($paramList);
+$fullUrl = "$baseUrl/$action?$queryString";
+
+$headers = getallheaders();
+foreach($headers as $key => &$value) {
+	switch($key) {
+		case 'User-Agent':
+			$value = 'uTorrent/2210(25110)';
+			break;
+		case 'Host':
+			$hostComponents = array_intersect_key(parse_url($fullUrl), ['host' => 0, 'port' => 0]);
+			$value = implode(':', $hostComponents);
+			break;
+	}
+}
+
+try {
+	$client = new Client();
+	$response = $client->request('GET', $fullUrl, [
+		'headers' => $headers,
+		'decode_content' => false,
+		'stream' => true
+	]);
+
+	foreach($response->getHeaders() as $headerKey => $headerValueList) {
+		foreach($headerValueList as $headerValue) {
+			header("$headerKey: $headerValue");
+		}
+	}
+	print $response->getBody()->getContents();
+}
+catch(RequestException $ex) {
+	http_response_code(400);
+}
