bdd4f66029934d7de0e925aa1a59afa3728c09fd
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2016-12-25 23:41:17 +0100

    Using Guzzle

--- code/php/speedtest_faker.php
+++ code/php/speedtest_faker.php
@@ -4,30 +4,33 @@
  * Generate custom tailored test summaries from speedtest.net
  */
 
+require 'vendor/autoload.php';
+
+use \GuzzleHttp\Client;
+
 $dlSpeed = @$argv[1] ? : exit;
 $ulSpeed = @$argv[2] ? : exit;
 $ping = @$argv[3] ? : exit;
 
 $id = 2231;
-$apiData = array(
-    'download' => $dlSpeed,
-    'ping' => $ping,
-    'upload' => $ulSpeed,
-    'promo' => '',
-    'startmode' => 'pingselect',
-    'recommendedserverid' => $id,
-    'accuracy' => 1,
-    'serverid' => $id,
-    'hash' => md5("$ping-$ulSpeed-$dlSpeed-297aae72")
-);
-$options = array(
-    'http' => array(
-        'method' => 'POST',
-        'header' => "Referer: http://c.speedtest.net/flash/speedtest.swf\r\nContent-type: application/x-www-form-urlencoded\r\n",
-        'content' => http_build_query($apiData)
-    )
-);
+$options = [
+	'headers' => [
+		'Referer' => 'http://c.speedtest.net/flash/speedtest.swf'
+	],
+	'form_params' => [
+		'download' => $dlSpeed,
+		'ping' => $ping,
+		'upload' => $ulSpeed,
+		'promo' => '',
+		'startmode' => 'pingselect',
+		'recommendedserverid' => $id,
+		'accuracy' => 1,
+		'serverid' => $id,
+		'hash' => md5("$ping-$ulSpeed-$dlSpeed-297aae72")
+	]
+];
 
-$response = file_get_contents('http://www.speedtest.net/api/api.php', false, stream_context_create($options));
-parse_str($response, $queryStringArgs);
-printf("http://www.speedtest.net/result/%s.png\n", $queryStringArgs['resultid']);
+$client = new Client();
+$response = $client->request('POST', 'http://www.speedtest.net/api/api.php', $options);
+parse_str($response->getBody(), $queryStringArgs);
+echo "http://www.speedtest.net/result/{$queryStringArgs['resultid']}.png\n";
