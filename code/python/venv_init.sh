823b60a5121a0047c507eed02c6df5c9aaf7edeb
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2019-01-14 01:56:59 +0100

    Dependencies in requirements.txt

--- code/python/venv_init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#cd "$(dirname "${BASH_SOURCE[0]}")"
-python3 -mvenv --system-site-packages venv
-. venv/bin/activate
-pip install --upgrade pip requests
-pip install bencode.py
