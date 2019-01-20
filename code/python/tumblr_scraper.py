080c4161a0e689ba0a038b6c4f7e15b5312eaabd
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2019-01-20 21:03:09 +0100

    Simpler tumblr_scraper.py

--- code/python/tumblr_scraper.py
+++ code/python/tumblr_scraper.py
@@ -12,22 +12,13 @@ from urllib import request
 def fetch_image(post_url, img_list):
     splitted_url = post_url.split('/')
     host = splitted_url[2].replace('.tumblr.com', '')
-    post_dir = '/'.join([host, splitted_url[4]])
-
-    try:
-        os.mkdir(host)
-    except OSError:
-        pass
-
-    try:
-        os.mkdir(post_dir)
-    except OSError:
-        pass
+    post_dir = os.path.join(host, splitted_url[4])
+    os.makedirs(post_dir, exist_ok = True)
 
     for img_url in img_list:
         img_url = re.sub(r'_(?:250|400|500|540|640)\.(jpg|png)', r'_1280.\1', img_url)
         file_name = re.sub(r'.*/', '', img_url)
-        file_path = '/'.join([post_dir, file_name])
+        file_path = os.path.join(post_dir, file_name)
         request.urlretrieve(img_url, file_path)
 
 
