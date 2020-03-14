ab33f4d78eccd6eb78af5c8323c1d1aa8dfe0b3d
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2020-03-14 23:57:30 +0100

    Updated lists

--- code/python/commands.sh
+++ code/python/commands.sh
@@ -1,12 +1,14 @@
 # list trackers from libtorrent files
-for i in $(find torrent -name "*.fastresume"); do
+find BT_backup -type f -name '*.fastresume' | read -r i; do
     ./torrent_parser.py < "$i"
 done | sort -u
 
 # check for dupes based on IP/SLD
-cat list/current.txt | egrep -o '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+|[^.:/]+\.[^.:/]+)($|:|/)' | sed -r 's@[:/]$@@' | sort | uniq -c
+egrep -o '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+|[^.:/]+\.[^.:/]+)($|:|/)' | sed -r 's@[:/]$@@'| sort | uniq -c
 
 # filter invalid trackers
-for i in $(seq 1 10); do
+for i in $(seq 1 4); do
     ./tracker_checker.py < list/current.txt
 done | sort -u
+
+comm -12 list/{old,good}.txt
