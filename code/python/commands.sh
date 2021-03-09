f9d2aaa6b32623fc4f599184e6c674a9825bd55d
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2021-03-09 21:42:00 +0100

    Updated requirements and commands

--- code/python/commands.sh
+++ code/python/commands.sh
@@ -1,14 +1,14 @@
 # list trackers from libtorrent files
-find BT_backup -type f -name '*.fastresume' | read -r i; do
-    ./torrent_parser.py < "$i"
+for i in BT_backup/*.fastresume; do
+    python3 -m torrent.parser < "$i"
 done | sort -u
 
 # check for dupes based on IP/SLD
-egrep -o '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+|[^.:/]+\.[^.:/]+)($|:|/)' | sed -r 's@[:/]$@@'| sort | uniq -c
+egrep -o '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+|[^.:/]+\.[^.:/]+)($|:|/)' | sed -r 's@[:/]$@@'| sort | uniq -c | awk '$1 > 1'
 
 # filter invalid trackers
 for i in $(seq 1 4); do
-    ./tracker_checker.py < list/current.txt
+    python3 -m torrent.tracker < in.txt
 done | sort -u
 
-comm -12 list/{old,good}.txt
+comm -23 {in,good}.txt > old.txt
