e0a2949661fa310719561167dba8fb121d8cd618
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2022-01-16 15:39:49 +0100

    Updated freebox.client

--- code/python/freebox/client.py
+++ code/python/freebox/client.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from importlib import resources
-from os import path
 import functools
 import hmac
+from importlib import resources
 import json
+from os import path
 import socket
 import sys
 import time
@@ -21,64 +21,62 @@ class BaseClient:
     CERTS_PATH = str(resources.path(__package__, 'certs.pem').__enter__())
 
     def __init__(self):
-        self.headers = None
+        self._hdrs = None
 
     def post(self, url, data=None):
         return requests.post(
             url,
             data=json.dumps(data) if data else None,
-            headers=self.headers,
+            headers=self._hdrs,
             verify=self.CERTS_PATH
         ).json()
 
     def get(self, url):
         return requests.get(
             url,
-            headers=self.headers,
+            headers=self._hdrs,
             verify=self.CERTS_PATH
         ).json()
 
     def discover(self):
-        response = self.get(f'{self.BASE_URL}/api_version')
-        base_path = response['api_base_url']
-        major_version = response['api_version'].split('.')[0]
-        return f'{self.BASE_URL}{base_path}v{major_version}'
+        resp = self.get(f'{self.BASE_URL}/api_version')
+        base = resp['api_base_url']
+        maj_ver = resp['api_version'].split('.')[0]
+        return f'{self.BASE_URL}{base}v{maj_ver}'
 
     def auth(self, session_token):
-        self.headers = {'X-Fbx-App-Auth': session_token}
+        self._hdrs = {'X-Fbx-App-Auth': session_token}
 
 
 class FreeboxOSClient:
-
     def __init__(self, app_id, app_name=None, app_token=None):
-        self.client = BaseClient()
-        self.api_url = self.client.discover()
-        self.app_id = app_id
-        self.app_name = app_name
-        self.app_token = app_token
-        self.track_id = None
+        self._clt = BaseClient()
+        self._url = self._clt.discover()
+        self._id = app_id
+        self._name = app_name
+        self._token = app_token
+        self._track = None
 
     def authorize(self):
-        response = self.client.post(f'{self.api_url}/login/authorize/', {
-            'app_id': self.app_id,
-            'app_name': self.app_name,
+        resp = self._clt.post(f'{self._url}/login/authorize/', {
+            'app_id': self._id,
+            'app_name': self._name,
             'device_name': socket.gethostname()
         })
 
-        if not response['success']:
-            raise FreeboxException(response)
+        if not resp['success']:
+            raise FreeboxException(resp)
 
-        self.track_id = str(response['result']['track_id'])
-        return response['result']['app_token']
+        self._track = str(resp['result']['track_id'])
+        return resp['result']['app_token']
 
     def track(self):
-        response = self.client.get(
-            f'{self.api_url}/login/authorize/{self.track_id}')
+        resp = self._clt.get(f'{self._url}/login/authorize/{self._track}')
 
-        if not response['success']:
-            raise FreeboxException(response)
+        if not resp['success']:
+            raise FreeboxException(resp)
 
-        return response['result']['status']
+        return resp['result']['status']
 
     def register(self, blocking=True):
         app_token = self.authorize()
@@ -92,67 +90,67 @@ class FreeboxOSClient:
             elif status != 'pending':
                 raise FreeboxException(status)
 
-        self.app_token = app_token
+        self._token = app_token
 
     def challenge(self):
-        response = self.client.get(f'{self.api_url}/login/')
+        resp = self._clt.get(f'{self._url}/login/')
 
-        if not response['success']:
-            raise FreeboxException(response)
+        if not resp['success']:
+            raise FreeboxException(resp)
 
-        return response['result']['challenge']
+        return resp['result']['challenge']
 
     def password(self):
         return hmac.new(
-            self.app_token.encode(),
+            self._token.encode(),
             msg=self.challenge().encode('utf-8'),
             digestmod='sha1'
         ).digest().hex()
 
     def session(self):
-        response = self.client.post(f'{self.api_url}/login/session', {
-            'app_id': self.app_id,
+        resp = self._clt.post(f'{self._url}/login/session', {
+            'app_id': self._id,
             'password': self.password()
         })
 
-        if not response['success']:
-            raise FreeboxException(response)
+        if not resp['success']:
+            raise FreeboxException(resp)
 
-        self.client.auth(response['result']['session_token'])
+        self._clt.auth(resp['result']['session_token'])
 
     def logout(self):
-        response = self.client.post(f'{self.api_url}/login/logout/')
+        resp = self._clt.post(f'{self._url}/login/logout/')
 
-        if not response['success']:
-            raise FreeboxException(response)
+        if not resp['success']:
+            raise FreeboxException(resp)
 
     def connection(self):
-        response = self.client.get(f'{self.api_url}/connection/')
+        resp = self._clt.get(f'{self._url}/connection/')
 
-        if not response['success']:
-            raise FreeboxException(response)
+        if not resp['success']:
+            raise FreeboxException(resp)
 
-        return response['result']
+        return resp['result']
 
     def lan_browser(self):
-        response = self.client.get(f'{self.api_url}/lan/browser/pub/')
+        resp = self._clt.get(f'{self._url}/lan/browser/pub/')
 
-        if not response['success']:
-            raise FreeboxException(response)
+        if not resp['success']:
+            raise FreeboxException(resp)
 
-        return response['result']
+        return resp['result']
 
     def host_count(self):
         return functools.reduce(lambda x, y: x + y['active'],
                                 self.lan_browser(), 0)
 
     def forwarding(self):
-        response = self.client.get(f'{self.api_url}/fw/redir/')
+        resp = self._clt.get(f'{self._url}/fw/redir/')
 
-        if not response['success']:
-            raise FreeboxException(response)
+        if not resp['success']:
+            raise FreeboxException(resp)
 
-        return response['result']
+        return resp['result']
 
     def self_forwarding(self):
         ip_list = socket.gethostbyname_ex(socket.gethostname())[-1]
@@ -175,12 +173,12 @@ if __name__ == '__main__':
                              'Freebox OS Client')
 
     if path.isfile(token_path):
-        with open(token_path) as f:
-            client.app_token = f.read().strip()
+        with open(token_path) as fp:
+            client._token = fp.read().strip()
     else:
-        with open(token_path, 'w') as f:
+        with open(token_path, 'w') as fp:
             client.register()
-            f.write(client.app_token)
+            fp.write(client._token)
 
     client.session()
 
