f7dfd268d60341badee4bacb82eed7ff1845adf7
Steve Forget <VonJagdstrumel@users.noreply.github.com>
2022-01-16 15:30:58 +0100

    Updated web_archiver.sh

--- code/shell/web_archiver.sh
+++ code/shell/web_archiver.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-url_encode() {
-    perl -MURI::Escape -e "print uri_escape(\$ARGV[0]);" -- "$1"
-}
-
 rm -f not_archived.txt archived.txt
 
-while read -r url; do
-    res=$(curl -s "https://archive.org/wayback/available?url=$(url_encode "$url")" | jq .archived_snapshots.closest.url)
+while IFS= read -r url; do
+    enc_url=$(perl -MURI::Escape -e 'print uri_escape(<>)' <<< "$url")
+    res=$(curl -s "https://archive.org/wayback/available?url=$enc_url" | jq .archived_snapshots.closest.url)
 
     if [[ $res == null ]]; then
-        printf '%4d NON %s\n' $((++i)) "$url"
+        printf '%4d Failure %s\n' $((++i)) "$url"
         echo "$url" >> not_archived.txt
         curl -s "https://web.archive.org/save/$url" > /dev/null
         sleep 4
     else
-        printf '%4d OUI %s\n' $((++i)) "$url"
+        printf '%4d Success %s\n' $((++i)) "$url"
         echo "\"$url\": $res," >> archived.txt
     fi
 done
